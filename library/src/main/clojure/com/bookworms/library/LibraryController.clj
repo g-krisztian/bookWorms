849322bb8df31420590cb346eb9e65877f65cb48
user user <user@workspace2pw2919qzb3i4qxv.tools-96455d6bc-g6cmj>
2019-10-09 17:19:40 +0000

    clojure is compiling

--- library/src/main/clojure/com/bookworms/library/LibraryController.clj
+++ library/src/main/clojure/com/bookworms/library/LibraryController.clj
@@ -1,8 +1,8 @@
-(ns library.controller
+(ns com.bookworms.library.LibraryController
   (:import org.springframework.stereotype.Controller
             (org.springframework.web.bind.annotation RequestMapping RequestMethod ResponseBody))
   (:gen-class
-    :name ^{Controller ""} com.bookworms.library.controller.LibraryController
+    :name ^{Controller ""} com.bookworms.library.controller
     :methods [[^{RequestMapping {:value ["/library"] :method [RequestMethod/GET]} ResponseBody {}} hello [] String]]))
 
 (defn -hello [this]
